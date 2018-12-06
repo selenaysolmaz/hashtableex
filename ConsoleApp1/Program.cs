9efecf50f7b58fbbedcf369257d60dc6b340b523
semvs <semvs@ayvansaray.edu.tr>
2018-12-06 10:57:11 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace ConsoleApp1
+{
+    class hashtable
+    {
+        static void Main()
+        {
+            Hashtable Sozluk = new Hashtable();
+            Sozluk.Add("araba", "car");
+            Sozluk["kitap"] = "book";
+            ICollection anahtarlar = Sozluk.Keys;
+            foreach (string s in anahtarlar)
+                Console.WriteLine(s);
+            ICollection degerler = Sozluk.Values;
+            foreach (string s in degerler)
+                Console.WriteLine(s);
+            Console.WriteLine("toplam kelime :" + Sozluk.Count);
+            Console.ReadKey();
+        }
+      
+    }
+   
+}
